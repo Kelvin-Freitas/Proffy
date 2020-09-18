aeb8a4e0d3ba9cd2b5d8ff60891222e2d28f777e
Kelvin Freitas <kelvin.arg11@gmail.com>
2020-09-18 18:24:09 -0300

    Landing page

--- mobile/src/pages/Landing/styles.ts
+++ mobile/src/pages/Landing/styles.ts
@@ -1,29 +1,59 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor: '#8257E5',
+    container: {
+        flex: 1,
+        backgroundColor: '#8257e5',
         justifyContent: 'center',
-        padding: 40
-    },
-    banner:{
+        padding: 40,
+      },
+      banner: {
         width: '100%',
-        //tive que definir height por não estar usando emulador
-        //ou expo no mobile, portanto dess forma a img surge no brownser
-        height: '30%', 
-        resizeMode: 'contain'
-    },
-    title:{
+        resizeMode: 'contain',
+      },
+      title: {
         fontFamily: 'Poppins_400Regular',
-        color: '#fff',
+        color: '#FFF',
         fontSize: 20,
         lineHeight: 30,
-        marginTop:80
-    },
-    titleBold:{
-        fontFamily: 'Poppins_600SemiBold'
-    }
+        marginTop: 40,
+      },
+      titleBold: {
+        fontFamily: 'Poppins_600SemiBold',
+      },
+      buttonsContainer: {
+        flexDirection: 'row',
+        marginTop: 30,
+        justifyContent: 'space-between',
+      },
+      button: {
+        height: 150,
+        width: '48%',
+        backgroundColor: '#333',
+        borderRadius: 8,
+        padding: 24,
+        justifyContent: 'space-between',
+      },
+      buttonPrimary: {
+        backgroundColor: '#9871f5',
+      },
+      
+      buttonSecondary: {
+        backgroundColor: '#04d361',
+      },
+      buttonText: {
+        color: '#fff',
+        fontFamily: 'Archivo_700Bold',
+        fontSize: 18,
+      },
+      totalConnections:{
+          fontFamily: 'Poppins_400Regular',
+          color: '#d4c2ff',
+          fontSize: 12,
+          lineHeight: 20,
+          maxWidth: 140,
+          marginTop: 20,
+      }
 });
 
 export default styles;
